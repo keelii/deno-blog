7af4f6fda1c11dce39b8b92454905670bda63d3e
keelii <keeliizhou@gmail.com>
2025-08-15 16:33:40 +0800

    feat: add default ENV values.

--- config.ts
+++ config.ts
@@ -1,14 +1,14 @@
 import { join, warn } from "./deps.ts";
 
-export const APP_PORT = Number(Deno.env.get("APP_PORT")) || 8000;
+export const APP_PORT = Number(Deno.env.get("APP_PORT")) || 80;
 export const APP_ENV = Deno.env.get("APP_ENV") || "prd";
 export const IS_PRD = APP_ENV === "prd";
 export const POST_DIR = join(Deno.cwd(), Deno.env.get("POST_DIR") || "blog");
-export const BLOG_URL = Deno.env.get("BLOG_URL") || "http://localhost";
-export const BLOG_TITLE = Deno.env.get("BLOG_TITLE") || "My Blog"
-export const BLOG_DESCRIPTION = Deno.env.get("BLOG_DESCRIPTION") || "Blog Description"
-export const BLOG_AUTHOR = Deno.env.get("BLOG_AUTHOR") || "unnamed"
-export const BLOG_KEYWORDS = Deno.env.get("BLOG_KEYWORDS") || "key1,key2,key3"
+export const BLOG_URL = Deno.env.get("BLOG_URL") || "https://keelii.com";
+export const BLOG_TITLE = Deno.env.get("BLOG_TITLE") || "臨池不輟"
+export const BLOG_DESCRIPTION = Deno.env.get("BLOG_DESCRIPTION") || "芝兰生于深林，不以无人而不芳，君子修道立德，不为穷困而败节。"
+export const BLOG_AUTHOR = Deno.env.get("BLOG_AUTHOR") || "keelii"
+export const BLOG_KEYWORDS = Deno.env.get("BLOG_KEYWORDS") || "前端开发,编程,javascript,typescript,css,html,nodejs,python,java"
 export const BLOG_RSS = Deno.env.get("BLOG_RSS") || "/atom.xml"
 
 warn("CONFIG:" + JSON.stringify({
